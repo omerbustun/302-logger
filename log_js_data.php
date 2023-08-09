426e814f127ef925b4396d5ee26f57bab1a21c2d
Ömer Üstün <omerberatustun@gmail.com>
2023-08-09 21:34:29 +0300

    Enhanced user tracking with cookies
    Implemented user identification via unique UIDs and added returning user detection using cookies.

--- log_js_data.php
+++ log_js_data.php
@@ -8,7 +8,7 @@
 
     // Create the log string with all data
     $timestamp = date("Y-m-d H:i:s");
-    $log = "Timestamp: $timestamp, IP Address: {$data->ip_address}, User-Agent: {$data->user_agent}, Request URI: {$data->request_uri}, Accept-Language: {$data->accept_language}, Referrer: {$data->referrer}, Screen Width: {$data->screen_width}, Screen Height: {$data->screen_height}, CPU Cores: {$data->cpu_cores}, Device Memory: {$data->device_memory}, Connection Type: {$data->connection_type}, Touch Support: {$data->touch_support} \n";
+    $log = "Timestamp: $timestamp, IP Address: {$data->ip_address}, User-Agent: {$data->user_agent}, Request URI: {$data->request_uri}, Accept-Language: {$data->accept_language}, Referrer: {$data->referrer}, Screen Width: {$data->screen_width}, Screen Height: {$data->screen_height}, CPU Cores: {$data->cpu_cores}, Device Memory: {$data->device_memory}, Connection Type: {$data->connection_type}, Touch Support: {$data->touch_support}, First Visit: {$data->first_visit}, Returning User: {$data->is_returning_user}, UID: {$data->user_uid} \n";
 
     file_put_contents($log_file, $log, FILE_APPEND);
 ?>
--- logger.php
+++ logger.php
@@ -4,21 +4,38 @@
     $redirect_url = $config['REDIRECT_URL'];
     $log_file = $config['LOG_FILE'];
 
+    if (!isset($_COOKIE['first_visit'])) {
+        setcookie("first_visit", time(), time() + (365 * 24 * 60 * 60), "/");  // expires in 1 year
+        $isReturningUser = false;
+    } else {
+        $isReturningUser = true;
+    }
+
+    // Check for UID or set a new one
+    if (!isset($_COOKIE['user_uid'])) {
+        $userUID = bin2hex(random_bytes(16)); // Generate a random UID
+        setcookie("user_uid", $userUID, time() + (365 * 24 * 60 * 60), "/");  // expires in 1 year
+    } else {
+        $userUID = $_COOKIE['user_uid'];
+    }
+
     // Get server-side data
-    $ip_address = $_SERVER['REMOTE_ADDR'] ?? '';
-    $user_agent = $_SERVER['HTTP_USER_AGENT'] ?? '';
-    $request_uri = $_SERVER['REQUEST_URI'] ?? '';
-    $accept_language = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '';
-    $referrer = $_SERVER['HTTP_REFERER'] ?? '';
+    $firstVisitTimestamp = $_COOKIE['first_visit'] ?? '';
+    $ip_address = $_SERVER['REMOTE_ADDR'] ?? 'unknown';
+    $user_agent = $_SERVER['HTTP_USER_AGENT'] ?? 'unknown';
+    $request_uri = $_SERVER['REQUEST_URI'] ?? 'unknown';
+    $accept_language = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? 'unknown';
+    $referrer = $_SERVER['HTTP_REFERER'] ?? 'unknown';
+    
 ?>
 <script>
     // Get client-side data
-    var screen_width = window.screen.width;
-    var screen_height = window.screen.height;
-    var cpu_cores = navigator.hardwareConcurrency;
-    var device_memory = navigator.deviceMemory;
-    var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
-    var touch_support = 'ontouchstart' in window;
+    var screen_width = window.screen.width || 'unknown';
+    var screen_height = window.screen.height || 'unknown';
+    var cpu_cores = navigator.hardwareConcurrency || 'unknown';
+    var device_memory = navigator.deviceMemory || 'unknown';
+    var connection = (navigator.connection && navigator.connection.effectiveType) || 'unknown';
+    var touch_support = ('ontouchstart' in window) || 'unknown';
 
     // Combine client-side and server-side data
     var combined_data = {
@@ -32,7 +49,11 @@
         user_agent: "<?php echo $user_agent; ?>",
         request_uri: "<?php echo $request_uri; ?>",
         accept_language: "<?php echo $accept_language; ?>",
-        referrer: "<?php echo $referrer; ?>"
+        referrer: "<?php echo $referrer; ?>",
+        first_visit: "<?php echo $firstVisitTimestamp; ?>",
+        is_returning_user: "<?php echo $isReturningUser ? 'yes' : 'no'; ?>",
+        user_uid: "<?php echo $userUID; ?>"
+
     };
 
     // Send combined data to server
